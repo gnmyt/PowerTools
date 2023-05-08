1a6698445ff1fbb21aae3a146121d8ec4becd3e5
Mathias Wagner <germannewsmaker@gmail.com>
2023-05-08 22:05:21 +0200

    Added Content & the Sidebar to the App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,20 @@
+import Sidebar from "@/common/components/Sidebar/index.js";
+import "@/common/styles/fonts.sass";
+import "@/common/styles/main.sass";
+
+const Content = () => {
+    return (
+        <>
+        <h1 style={{margin: 0}}>Content</h1>
+        </>
+    )
+}
+
 const App = () => {
   return (
     <>
+      <Sidebar />
+      <Content />
     </>
   )
 }
