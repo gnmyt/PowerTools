b94bde6031685d0fca735d0a85cbda5ef68a4d9b
Mathias Wagner <germannewsmaker@gmail.com>
2023-05-08 22:04:43 +0200

    Created the Sidebar.jsx

---
+++ client/src/common/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,10 @@
+import SidebarHeader from "@/common/components/Sidebar/components/SidebarHeader";
+import "./styles.sass";
+
+export const Sidebar = () => {
+    return (
+        <aside>
+            <SidebarHeader />
+        </aside>
+    )
+}
