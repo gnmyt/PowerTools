2eb34f2244efce8cec34949c724cc90b51b43006
Mathias Wagner <germannewsmaker@gmail.com>
2023-05-30 19:00:25 +0200

    Updated the App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,17 +4,20 @@ import "@/common/styles/main.sass";
 import {BrowserRouter} from "react-router-dom";
 import Header from "@/common/components/Header";
 import Content from "@/common/components/Content";
+import {useState} from "react";
 
 const App = () => {
-  return (
-    <>
-        <BrowserRouter>
-            <Sidebar />
-            <Header />
-            <Content />
-        </BrowserRouter>
-    </>
-  )
+    const [sidebarOpen, setSidebarOpen] = useState(window.innerHeight >= 1024);
+
+    return (
+        <>
+            <BrowserRouter>
+                <Sidebar open={sidebarOpen} setOpen={setSidebarOpen}/>
+                <Header open={sidebarOpen} setOpen={setSidebarOpen} />
+                <Content/>
+            </BrowserRouter>
+        </>
+    )
 }
 
 export default App;
