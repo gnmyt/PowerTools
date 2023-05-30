015b7dc5298c9bd9108568052f85b90c25743ddd
Mathias Wagner <germannewsmaker@gmail.com>
2023-05-31 01:57:45 +0200

    Added a dynamic title to the Content.jsx

--- client/src/common/components/Content/Content.jsx
+++ client/src/common/components/Content/Content.jsx
@@ -1,13 +1,21 @@
 import "./styles.sass";
-import {routes} from "@/common/routes";
-import {Route, Routes} from "react-router-dom";
+import {getByPath, routes} from "@/common/routes";
+import {Route, Routes, useLocation} from "react-router-dom";
+import {useEffect} from "react";
 
-export const Content = () => (
-    <div className="content-wrapper">
-        <Routes>
-            {Object.keys(routes).map((route) => (routes[route].map((route) => (
-                <Route exact path={route.path} element={route.component}/>
-            ))))}
-        </Routes>
-    </div>
-)
+export const Content = () => {
+    const location = useLocation();
+
+    useEffect(() => {
+        document.title = "PowerTools - " + (getByPath(location.pathname)?.name || "404");
+    }, [location]);
+
+    return (
+        <div className="content-wrapper">
+            <Routes>
+                {Object.keys(routes).map((route) => (routes[route].map((route) => (
+                    <Route exact path={route.path} element={route.component}/>
+                ))))}
+            </Routes>
+        </div>);
+}
