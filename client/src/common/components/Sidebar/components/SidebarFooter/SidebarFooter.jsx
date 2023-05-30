8375be033821028f4cb7d1fb976c4fd2d913dc3a
Mathias Wagner <germannewsmaker@gmail.com>
2023-05-30 18:14:04 +0200

    Created the SidebarFooter.jsx

---
+++ client/src/common/components/Sidebar/components/SidebarFooter/SidebarFooter.jsx
@@ -0,0 +1,9 @@
+import "./styles.sass";
+
+export const SidebarFooter = () => (
+    <div className="sidebar-footer">
+        <p>Privacy</p>
+        <p>・</p>
+        <p>Imprint</p>
+    </div>
+)
