7403402df592ba598a320dd90e81361a24435b6c
Mathias Wagner <germannewsmaker@gmail.com>
2023-05-30 19:00:06 +0200

    Updated the SidebarHeader.jsx

--- client/src/common/components/Sidebar/components/SidebarHeader/SidebarHeader.jsx
+++ client/src/common/components/Sidebar/components/SidebarHeader/SidebarHeader.jsx
@@ -1,20 +1,23 @@
 import Logo from "@/common/assets/logo.png";
 import "./styles.sass";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faBolt} from "@fortawesome/free-solid-svg-icons";
+import {faBars, faBolt} from "@fortawesome/free-solid-svg-icons";
 
-export const SidebarHeader = () => {
+export const SidebarHeader = ({setOpen}) => {
     return (
-        <div className="sidebar-header">
-            <img src={Logo} alt="Logo" />
-            <div className="header-title">
-                <div className="title-header">
-                    <p>POWER</p> <FontAwesomeIcon icon={faBolt} />
-                </div>
-                <div className="title-subheader">
-                    <p>TOOLS <span>by GNM</span></p>
+        <div className="sidebar-header header-hidden">
+            <div className="sidebar-left">
+                <img src={Logo} alt="Logo" />
+                <div className="header-title">
+                    <div className="title-header">
+                        <p>POWER</p> <FontAwesomeIcon icon={faBolt} />
+                    </div>
+                    <div className="title-subheader">
+                        <p>TOOLS <span>by GNM</span></p>
+                    </div>
                 </div>
             </div>
+            <FontAwesomeIcon icon={faBars} className="close-menu" onClick={() => setOpen(false)} />
         </div>
     )
 }
