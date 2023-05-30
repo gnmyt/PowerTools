1d7ca252d4f77fff0ef7d33bc8fe220b0e1404d4
Mathias Wagner <germannewsmaker@gmail.com>
2023-05-30 20:06:09 +0200

    Updated the Sidebar.jsx

--- client/src/common/components/Sidebar/Sidebar.jsx
+++ client/src/common/components/Sidebar/Sidebar.jsx
@@ -2,11 +2,19 @@ import SidebarHeader from "@/common/components/Sidebar/components/SidebarHeader"
 import "./styles.sass";
 import SidebarContent from "@/common/components/Sidebar/components/SidebarContent";
 import SidebarFooter from "@/common/components/Sidebar/components/SidebarFooter";
+import {useRef} from "react";
 
-export const Sidebar = ({open, setOpen}) => (
-    <aside className={open ? "" : "sidebar-hidden"}>
-        <SidebarHeader setOpen={setOpen}/>
-        <SidebarContent />
-        <SidebarFooter />
-    </aside>
-)
+export const Sidebar = ({open, setOpen}) => {
+    const ref = useRef();
+
+    return (
+        <>
+            {open && <div className="background-overlay" ref={ref} onClick={() => setOpen(false)}/>}
+            <aside className={open ? "" : "sidebar-hidden"} ref={ref}>
+                <SidebarHeader setOpen={setOpen}/>
+                <SidebarContent/>
+                <SidebarFooter/>
+            </aside>
+        </>
+    );
+}
