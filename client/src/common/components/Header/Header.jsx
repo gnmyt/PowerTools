7c471d97a7a3e435a643f576c9ed7190538c9002
Mathias Wagner <germannewsmaker@gmail.com>
2023-05-30 18:59:53 +0200

    Updated the Header.jsx

--- client/src/common/components/Header/Header.jsx
+++ client/src/common/components/Header/Header.jsx
@@ -1,16 +1,19 @@
 import "./styles.sass";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faGear} from "@fortawesome/free-solid-svg-icons";
+import {faBars, faGear} from "@fortawesome/free-solid-svg-icons";
 import {getByPath} from "@/common/routes";
 import {useLocation} from "react-router-dom";
 
-export const Header = () => {
+export const Header = ({open, setOpen}) => {
     const location = useLocation();
     const path = getByPath(location.pathname);
 
     return (
         <div className="header">
-            <h1>{path.category} / <span>{path.name}</span></h1>
+            <div className="header-left">
+                {!open && <FontAwesomeIcon icon={faBars} className="open-menu" onClick={() => setOpen(true)}/>}
+                <h1>{path?.category} / <span>{path?.name}</span></h1>
+            </div>
             <FontAwesomeIcon icon={faGear} />
         </div>
     );
