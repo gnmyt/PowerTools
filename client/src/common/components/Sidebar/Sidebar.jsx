91ec9ecd39ff03e4237340e5febcd9a25ca57ad7
Mathias Wagner <germannewsmaker@gmail.com>
2023-05-30 18:15:10 +0200

    Added the SidebarContent & SidebarFooter to the Sidebar.jsx

--- client/src/common/components/Sidebar/Sidebar.jsx
+++ client/src/common/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,14 @@
 import SidebarHeader from "@/common/components/Sidebar/components/SidebarHeader";
 import "./styles.sass";
+import SidebarContent from "@/common/components/Sidebar/components/SidebarContent";
+import SidebarFooter from "@/common/components/Sidebar/components/SidebarFooter";
 
 export const Sidebar = () => {
     return (
         <aside>
             <SidebarHeader />
+            <SidebarContent />
+            <SidebarFooter />
         </aside>
     )
 }
