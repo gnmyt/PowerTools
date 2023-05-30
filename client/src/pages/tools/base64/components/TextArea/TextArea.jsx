d92c4e8f69ec4c7bd5e9ca452c96aa10f016fdc2
Mathias Wagner <germannewsmaker@gmail.com>
2023-05-31 01:34:53 +0200

    Added the placeholder attribute to the TextArea.jsx

--- client/src/pages/tools/base64/components/TextArea/TextArea.jsx
+++ client/src/pages/tools/base64/components/TextArea/TextArea.jsx
@@ -1,5 +1,5 @@
 import "./styles.sass";
 
-export const TextArea = ({value, onChange, readOnly = false}) => (
-    <textarea className="area" rows="10" value={value} onChange={onChange} readOnly={readOnly} />
+export const TextArea = ({value, onChange, readOnly = false, placeholder}) => (
+    <textarea className="area" rows="10" value={value} onChange={onChange} readOnly={readOnly} placeholder={placeholder} />
 )
