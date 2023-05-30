368da472fe1f04b12f58e4827890f042b3c335b8
Mathias Wagner <germannewsmaker@gmail.com>
2023-05-30 19:03:23 +0200

    Updated the Sidebar.jsx

--- client/src/common/components/Sidebar/Sidebar.jsx
+++ client/src/common/components/Sidebar/Sidebar.jsx
@@ -3,12 +3,10 @@ import "./styles.sass";
 import SidebarContent from "@/common/components/Sidebar/components/SidebarContent";
 import SidebarFooter from "@/common/components/Sidebar/components/SidebarFooter";
 
-export const Sidebar = ({open, setOpen}) => {
-    return (
-        <aside className={open ? "" : "sidebar-hidden"}>
-            <SidebarHeader setOpen={setOpen}/>
-            <SidebarContent />
-            <SidebarFooter />
-        </aside>
-    )
-}
+export const Sidebar = ({open, setOpen}) => (
+    <aside className={open ? "" : "sidebar-hidden"}>
+        <SidebarHeader setOpen={setOpen}/>
+        <SidebarContent />
+        <SidebarFooter />
+    </aside>
+)
