09b79c66bf0e25ae2d63f135bec603c0743940df
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-01 19:44:50 +0200

    Added a warning to the Home page if the backend is unavailable

--- client/src/pages/tools/general/Home/Home.jsx
+++ client/src/pages/tools/general/Home/Home.jsx
@@ -2,23 +2,33 @@ import "./styles.sass";
 import {faFire, faKeyboard, faTerminal, faUnlock} from "@fortawesome/free-solid-svg-icons";
 import HomeTool from "@/pages/tools/general/Home/components/HomeTool";
 import HomeElement from "@/pages/tools/general/Home/components/HomeElement";
+import ErrorArea from "@/common/components/ErrorArea/index.js";
+import {useContext} from "react";
+import {StatusContext} from "@/common/contexts/Status/index.js";
 
-export const Home = () => (
-    <>
-        <h1 className="home-header">Hi!</h1>
-        <p className="home-subtext">Diese Webseite wurde erstellt um viele alltägliche Aufgaben (wie z. B. das
-            Installieren eines Minecraft-Servers oder updaten einer Software) auf einer Seite zu vereinen und in Tools
-            zu unterteilen. Probier’s doch einfach mal aus!</p>
-        <div className="tool-area">
-            <HomeElement icon={faFire} text="Beliebte Tools" />
+export const Home = () => {
+    const backendAvailable = useContext(StatusContext);
 
-            <div className="tool-list">
-                <HomeTool icon={faTerminal} title="Remote-SSH" description="Baue über das Web eine Sitzung mit deinem Linux-Server auf" />
-                <HomeTool icon={faUnlock} title="Base64 Decode" description="Entschlüssel ganz einfach Base64-Kodierte Strings" />
+    return (
+        <>
+            {!backendAvailable && <ErrorArea error="Es konnte keine Verbindung zum Backend-Server hergestellt werden."/>}
+            <h1 className="home-header">Hi!</h1>
+            <p className="home-subtext">Diese Webseite wurde erstellt um viele alltägliche Aufgaben (wie z. B. das
+                Installieren eines Minecraft-Servers oder updaten einer Software) auf einer Seite zu vereinen und in
+                Tools zu unterteilen. Probier’s doch einfach mal aus!</p>
+            <div className="tool-area">
+                <HomeElement icon={faFire} text="Beliebte Tools"/>
+
+                <div className="tool-list">
+                    <HomeTool icon={faTerminal} title="Remote-SSH"
+                              description="Baue über das Web eine Sitzung mit deinem Linux-Server auf"/>
+                    <HomeTool icon={faUnlock} title="Base64 Decode"
+                              description="Entschlüssel ganz einfach Base64-Kodierte Strings"/>
+                </div>
             </div>
-        </div>
-        <div className="shortcut-area">
-            <HomeElement icon={faKeyboard} text="Shortcuts" />
-        </div>
-    </>
-)
+            <div className="shortcut-area">
+                <HomeElement icon={faKeyboard} text="Shortcuts"/>
+            </div>
+        </>
+    );
+}
