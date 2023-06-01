b999a7335150a0b3f3ba08fbe7bfedf3ca2b9b7f
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-01 19:41:45 +0200

    Created the StatusContext.jsx

---
+++ client/src/common/contexts/Status/StatusContext.jsx
@@ -0,0 +1,29 @@
+import React, {createContext, useEffect, useState} from "react";
+import {jsonRequest} from "@/common/utils/RequestUtil";
+
+export const StatusContext = createContext({});
+
+export const StatusProvider = (props) => {
+
+    const [backendAvailable, setBackendAvailable] = useState(true);
+
+    const updateStatus = () => jsonRequest("").then(result => {
+        if (result?.status !== "ok") {
+            setBackendAvailable(false);
+        } else {
+            setBackendAvailable(true);
+        }
+    });
+
+    useEffect(() => {
+        updateStatus();
+        const interval = setInterval(() => updateStatus(), 5000);
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <StatusContext.Provider value={backendAvailable}>
+            {props.children}
+        </StatusContext.Provider>
+    )
+}
