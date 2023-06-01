d0c8c682c81c5152bad829fde5c39f24e142bf0d
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-01 19:40:55 +0200

    Renamed socket.js -> SocketUtil.js

--- client/src/common/utils/SocketUtil.js
+++ client/src/common/utils/SocketUtil.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = process.env.NODE_ENV === 'production' ? "https://tools-api.gnmyt.dev/" : "http://localhost:7182";
+const URL = process.env.NODE_ENV === 'production' ? "https://tools-api.gnmyt.dev/" : "http://localhost:7182/";
 
 export const createConnection = (type) => {
     const socket = io(URL, {autoConnect: false});
