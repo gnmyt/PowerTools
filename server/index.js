622e7e41de99020026766724a87e27b10359558e
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-01 18:10:35 +0200

    Integrated socket.io into the server index

--- server/index.js
+++ server/index.js
@@ -1,13 +1,27 @@
-const express = require('express');
+const express = require("express");
+const http = require("http");
+const {Server} = require("socket.io");
+const handle = require("./handler/socket");
 
 const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {cors: {origin: "*",}});
+
+io.on("connection", (socket) => handle(io, socket));
 
 app.disable('x-powered-by');
 
 const port = process.env.port || 7182;
 
+// Register error handlers
+app.use(require("./middlewares/error"));
+
 // Register middlewares
 app.use(express.json());
 
+// Register routes
+app.get("/", (req, res) => res.json({status: "ok"}));
+
 // Start the server
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+server.listen(port, () => console.log(`Server listening on port ${port}`));
