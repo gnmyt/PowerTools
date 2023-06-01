78675d385bc41ee9556d9f3cb192558fd6799511
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-02 00:41:38 +0200

    Added the docker entry to the software.jsx

--- client/src/pages/tools/linux/InstallSoftware/software.jsx
+++ client/src/pages/tools/linux/InstallSoftware/software.jsx
@@ -1,4 +1,5 @@
 import MySpeedImage from "./images/myspeed.webp";
+import DockerImage from "./images/docker.webp";
 import {faBox, faFolder} from "@fortawesome/free-solid-svg-icons";
 
 export default [
@@ -9,6 +10,14 @@ export default [
             {type: "input", id: "folder", text: "Ort", icon: faFolder, value: "/opt/myspeed"},
             {type: "select", id: "environment", text: "Umgebung", icon: faBox, options: ["Stabil", "Entwicklung"],
                 value: "Stabil"}
+        ]
+    },
+    {
+        name: "Docker",
+        icon: DockerImage,
+        configuration: [
+            {type: "select", id: "environment", text: "Umgebung", icon: faBox, options: ["Stabil", "Test"],
+                value: "Stabil"},
         ]
     }
 ];
