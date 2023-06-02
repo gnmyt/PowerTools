61fdf92b9bb75271cb6f7b27c6b70b96eb71bc65
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-03 01:37:24 +0200

    Integrated the ServerDialog into the RemoteSSH.jsx

--- client/src/pages/tools/linux/RemoteSSH/RemoteSSH.jsx
+++ client/src/pages/tools/linux/RemoteSSH/RemoteSSH.jsx
@@ -3,17 +3,23 @@ import Button from "@/common/components/Button";
 import {faGear} from "@fortawesome/free-solid-svg-icons";
 import ErrorArea from "@/common/components/ErrorArea";
 import "./styles.sass";
+import {useState} from "react";
+import ServerDialog from "@/pages/tools/linux/components/ServerDialog/index.js";
+import {ServerProvider} from "@/common/contexts/Server/index.js";
 
 export const RemoteSSH = () => {
+    const [serverDialogOpen, setServerDialogOpen] = useState(false);
 
     return (
-        <>
+        <ServerProvider>
             <InfoArea title="Remote SSH" description="Mit diesem Tool kannst du dich mit einem SSH-Server verbinden und diesen fernsteuern.">
-                <Button icon={faGear} text="Konfigurieren" onClick={() => {}}/>
+                <Button icon={faGear} text="Konfigurieren" onClick={() => setServerDialogOpen(true)}/>
             </InfoArea>
 
+            {serverDialogOpen && <ServerDialog onClose={() => setServerDialogOpen(false)}/>}
+
             <ErrorArea error="Dieses Tool ist noch nicht verfügbar"/>
-        </>
+        </ServerProvider>
     );
 
 }
