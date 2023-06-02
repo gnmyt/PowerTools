6301b1e8272d08dab582b3c556e6d049d5291bc5
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-03 01:37:20 +0200

    Integrated the ServerDialog into the RemoteSFTP.jsx

--- client/src/pages/tools/linux/RemoteSFTP/RemoteSFTP.jsx
+++ client/src/pages/tools/linux/RemoteSFTP/RemoteSFTP.jsx
@@ -3,17 +3,24 @@ import Button from "@/common/components/Button";
 import {faGear} from "@fortawesome/free-solid-svg-icons";
 import ErrorArea from "@/common/components/ErrorArea";
 import "./styles.sass";
+import ServerDialog from "@/pages/tools/linux/components/ServerDialog/index.js";
+import {useState} from "react";
+import {ServerProvider} from "@/common/contexts/Server/index.js";
 
 export const RemoteSFTP = () => {
 
+    const [serverDialogOpen, setServerDialogOpen] = useState(false);
+
     return (
-        <>
+        <ServerProvider>
             <InfoArea title="Remote SFTP" description="Mit diesem Tool kannst du deine Dateien auf einem SFTP-Server verwalten">
-                <Button icon={faGear} text="Konfigurieren" onClick={() => {}}/>
+                <Button icon={faGear} text="Konfigurieren" onClick={() => setServerDialogOpen(true)}/>
             </InfoArea>
 
+            {serverDialogOpen && <ServerDialog onClose={() => setServerDialogOpen(false)}/>}
+
             <ErrorArea error="Dieses Tool ist noch nicht verfügbar"/>
-        </>
+        </ServerProvider>
     );
 
 }
