917dec35d0ab9c5e2cbf1e1bd3d8416ab28ae0a8
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-02 20:20:47 +0200

    Integrated the ColorDialog into the Header.jsx

--- client/src/common/components/Header/Header.jsx
+++ client/src/common/components/Header/Header.jsx
@@ -3,18 +3,23 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faBars, faGear} from "@fortawesome/free-solid-svg-icons";
 import {getByPath} from "@/common/routes";
 import {useLocation} from "react-router-dom";
+import {useState} from "react";
+import ColorDialog from "@/common/components/Header/components/ColorDialog/index.js";
 
-export const Header = ({open, setOpen}) => {
+export const Header = ({open, setOpen, color, setColor}) => {
     const location = useLocation();
     const path = getByPath(location.pathname);
 
+    const [dialogOpen, setDialogOpen] = useState(false);
+
     return (
         <div className="header">
+            {dialogOpen && <ColorDialog onClose={() => setDialogOpen(false)} color={color} setColor={setColor}/>}
             <div className="header-left">
                 {!open && <FontAwesomeIcon icon={faBars} className="open-menu" onClick={() => setOpen(true)}/>}
                 <h1>{path?.category} / <span>{path?.name}</span></h1>
             </div>
-            <FontAwesomeIcon icon={faGear} />
+            <FontAwesomeIcon icon={faGear} onClick={() => setDialogOpen(true)} />
         </div>
     );
 }
