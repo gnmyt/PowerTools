15c81f01bc146a1535dacc7c92d064c655489c8a
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-03 01:35:30 +0200

    Fixed a bug in the App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
 
     useEffect(() => {
         document.documentElement.style.setProperty("--primary", primaryColor);
+        localStorage.setItem("primary-color", primaryColor);
     }, [primaryColor]);
 
     return (
