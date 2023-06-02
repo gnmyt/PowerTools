da91440034d7651ab986747868efcccc5f46a786
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-02 17:00:13 +0200

    Implemented the browser router in the main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {routes} from "@/common/routes/index.jsx";
+
+const router = createBrowserRouter([{
+    path: "/",
+    element: <App/>,
+    children: Object.keys(routes).map(key => routes[key]).flat(Infinity).map(route => ({
+        index: route.path === '/',
+        path: route.path === '/' ? undefined : route.path,
+        element: route.component
+    }))
+}]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>,
 );
