d734b0710911e512aac64fcbaa4fd30cff0466b2
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-03 01:35:00 +0200

    Added value keys to the ConfigurationRow.jsx

--- client/src/pages/tools/linux/InstallSoftware/components/ConfigurationRow/ConfigurationRow.jsx
+++ client/src/pages/tools/linux/InstallSoftware/components/ConfigurationRow/ConfigurationRow.jsx
@@ -11,7 +11,10 @@ export const ConfigurationRow = ({text, icon = faSquareRootVariable, type = "inp
             </div>
 
             {type === "select" && <select className="input-field" value={value} onChange={(e) => setValue(e.target.value)}>
-                {options.map((option) => <option key={option}>{option}</option>)}
+                {options.map((option) =>
+                    option.value ? <option value={option.value} key={option.value}>{option.text}</option>
+                        : <option key={option}>{option}</option>
+                )}
             </select>}
 
             {type === "input" && <input className="input-field" type="text" placeholder={text} value={value}
