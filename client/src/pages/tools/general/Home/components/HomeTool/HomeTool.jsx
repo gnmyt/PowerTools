8926b1331723baf531d9627f4bcbaa02cd1193c5
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-03 16:30:36 +0200

    Implemented on Click in the HomeTool.jsx

--- client/src/pages/tools/general/Home/components/HomeTool/HomeTool.jsx
+++ client/src/pages/tools/general/Home/components/HomeTool/HomeTool.jsx
@@ -1,8 +1,8 @@
 import "./styles.sass";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
-export const HomeTool = ({icon, title, description}) => (
-    <div className="home-tool">
+export const HomeTool = ({icon, title, description, onClick}) => (
+    <div className="home-tool" onClick={onClick}>
         <FontAwesomeIcon icon={icon} />
         <h1>{title}</h1>
         <p>{description}</p>
