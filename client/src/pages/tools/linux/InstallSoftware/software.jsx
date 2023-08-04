a2e0b513d742a823cd301f8b312eb58e6e32695a
Mathias Wagner <germannewsmaker@gmail.com>
2023-08-04 18:09:44 +0200

    Added pterodactyl to the software.jsx

--- client/src/pages/tools/linux/InstallSoftware/software.jsx
+++ client/src/pages/tools/linux/InstallSoftware/software.jsx
@@ -2,7 +2,16 @@ import UpdateImage from "./images/update.webp";
 import MySpeedImage from "./images/myspeed.webp";
 import DockerImage from "./images/docker.webp";
 import NextCloudImage from "./images/nextcloud.webp";
-import {faBox, faFolder, faGlobe, faInfoCircle, faKey, faUser} from "@fortawesome/free-solid-svg-icons";
+import PterodactylImage from "./images/pterodactyl.webp";
+import {
+    faBox, faEnvelope,
+    faFolder,
+    faGlobe,
+    faInfoCircle,
+    faKey,
+    faLanguage, faMailBulk, faServer,
+    faUser
+} from "@fortawesome/free-solid-svg-icons";
 
 export default [
     {
@@ -38,6 +47,18 @@ export default [
             {type: "input", id: "domain", text: "Domain", icon: faGlobe, value: "cloud.example.com"},
             {type: "input", id: "user", text: "Benutzer", icon: faUser, value: "Admin"},
             {type: "input", id: "pass", text: "Passwort", icon: faKey, value: () => Math.random().toString(36).slice(-8)}
+        ]
+    },
+    {
+        name: "Pterodactyl",
+        icon: PterodactylImage,
+        configuration: [
+            {type: "input", id: "folder", text: "Ort", icon: faFolder, value: "/var/www/pterodactyl"},
+            {type: "input", id: "domain", text: "Domain", icon: faGlobe, value: "pterodactyl.example.com"},
+            {type: "select", id: "german", text: <a href="https://germandactyl.de" target="_blank">GermanDactyl</a>,
+                icon: faLanguage, options: ["Ja", "Nein"], value: "Ja"},
+            {type: "input", id: "mail", text: "E-Mail", icon: faEnvelope, value: "unknown@example.com"},
+            {type: "input", id: "pass", text: "Passwort", icon: faKey, value: () => Math.random().toString(36).slice(-8)},
         ]
     }
 ];
