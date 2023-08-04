01c6567599980d788da7fa73b070467390ce7e00
Mathias Wagner <germannewsmaker@gmail.com>
2023-08-04 12:04:53 +0200

    Created the Imprint.jsx legal page

---
+++ client/src/pages/legal/Imprint/Imprint.jsx
@@ -0,0 +1,15 @@
+import "./styles.sass";
+
+export const Imprint = () => {
+    return (
+        <>
+            <div className="imprint-area">
+                <h1 className="home-header">Impressum</h1>
+                <h2 className="home-subtext">Angaben gemäß § 5 TMG</h2>
+
+                <p>Mathias Wagner<br/>c/o IP-Management #14358<br/>Ludwig-Erhard-Str. 18<br/>20459 Hamburg</p>
+                <h2>Kontakt</h2><p>E-Mail: mathias@gnmyt.dev</p><p>Quelle: <a href="https://www.e-recht24.de" target="_blank">eRecht24</a></p>
+            </div>
+        </>
+    )
+}
