e10817a13e3206b8b4e54af6dfde017ce993d87d
Mathias Wagner <germannewsmaker@gmail.com>
2023-08-05 22:29:47 +0200

    Added the servers.js

---
+++ client/src/common/components/Header/components/ServerDialog/servers.js
@@ -0,0 +1,18 @@
+import GermanyFlag from "./flags/de.webp";
+
+export default [
+    {
+        "url": "https://tools-api.gnmyt.dev/",
+        "name": "Hauptserver (Nürnberg)",
+        "provider": "Paul Schwarz\nschwarzpaul123@gmail.com",
+        "location": "Hetzner Online GmbH\nSigmundstraße 135\n90431 Nürnberg",
+        "img": GermanyFlag
+    },
+    {
+        "url": "https://tools-api-prod-2.gnmyt.dev/",
+        "name": "Zweitserver (Nürnberg)",
+        "provider": "Mathias Wagner\nmathias@gnmyt.dev",
+        "location": "Hetzner Online GmbH\nSigmundstraße 135\n90431 Nürnberg",
+        "img": GermanyFlag
+    }
+];
