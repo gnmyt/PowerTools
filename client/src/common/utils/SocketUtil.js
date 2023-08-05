fd78c88ef311af40007f8c6ee05d380c78987069
Mathias Wagner <germannewsmaker@gmail.com>
2023-08-05 21:53:13 +0200

    Updated the SocketUtil.js

--- client/src/common/utils/SocketUtil.js
+++ client/src/common/utils/SocketUtil.js
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client';
 
-const URL = process.env.NODE_ENV === 'production' ? "https://tools-api.gnmyt.dev/" : "http://localhost:7182/";
+const URL = process.env.NODE_ENV === 'production' ? (localStorage.getItem("url") || "https://tools-api.gnmyt.dev/")
+    : "http://localhost:7182/";
 
 export const createConnection = (type) => {
     const socket = io(URL, {autoConnect: false});
